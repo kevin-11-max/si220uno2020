e8e9f5076b0401d31a07913c7770aa0f6181d732
kevin-11-max <kevinhenicken@gmail.com>
2020-02-22 11:30:25 -0400

    creacion de webapi completo

---
+++ solUno/webapiUno/Models/DataContex.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace webapiUno.Models
+{
+    using System.Data.Entity;
+
+    public class DataContex:DbContext
+    {
+        public DataContex():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<webapiUno.Models.Person> People { get; set; }
+    }
+}
---
+++ solUno/webapiUno/Models/person.cs
@@ -0,0 +1,18 @@
+﻿namespace webapiUno.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    public class Person
+    {
+        [Key]
+        public int PersonID { get; set; }
+
+        [Required(ErrorMessage = "El nombre es requerido")]
+        [Display(Name = "Nombre completo")]
+        [StringLength(100, ErrorMessage = "Maximo 100 caracteres")]
+        public string Name { get; set; }
+
+        [Display(Name = "carnet de identidad")]
+        public string Document { get; set; }
+
+    }
+}
